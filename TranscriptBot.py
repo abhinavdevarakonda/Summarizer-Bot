fef1a73e642a54d1e8386f01793041baf931e198
abhinavdevarakonda <50144533+abhinavdevarakonda@users.noreply.github.com>
2022-01-29 22:04:24 +0530

    Add files via upload

---
+++ TranscriptBot.py
@@ -0,0 +1,45 @@
+import discord
+
+from yttranscript import summarizer
+
+
+
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        global video_link
+        # we do not want the bot to reply to itself
+
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!help '):
+            await message.channel.send('1.  !summarize <youtube-video-link> ---  to summarize transcript of youtube video provided.')
+
+        if message.content.startswith('!summarize '):
+            s = message.content.split()
+            video_link = s[1]
+            print(video_link)
+            await message.channel.send('Hello, {0.author.mention}! summarizing youtube video...'.format(message))
+            StringFormContent = ""
+            for i in summarizer(video_link):
+                StringFormContent += i
+            
+            if len(StringFormContent) < 2000:
+                await message.channel.send(StringFormContent)
+            
+            else:
+                await message.channel.send(StringFormContent[:len(StringFormContent)//2])
+                await message.channel.send(StringFormContent[len(StringFormContent//2):])
+
+
+
+client = MyClient()
+client.run('OTM1NzM5MDQ3MDc3MzcxOTA0.YfDA5g.vJcPBd9i9CrI5H27xVJnAuoCkXs')
---
+++ yttranscript.py
@@ -0,0 +1,55 @@
+from transformers import pipeline
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def summarizer(link):
+    VIDEO = link
+
+    #a youtube videos id is need, which is just the part of the url after the "=".
+
+    VideoId = VIDEO[(VIDEO.index('=')+1):]
+
+
+    #getting transcript of video using YoutubeTranscriptApi
+    transcript = YouTubeTranscriptApi.get_transcript(VideoId)
+
+
+
+    '''
+    [{'text': "i'm gonna show you how to become a", 'start': 0.0, 'duration': 3.84}, {'text': 'millionaire all you need', 'start': 0.96, 'duration': 5.52}, {'text': 'is to have one thing that you can get', 'start': 3.84, 'duration': 3.519}, 
+    {'text': 'ten dollars for', 'start': 6.48, 'duration': 1.92}, {'text': "i don't care if it's cutting hair", 'start': 7.359, 'duration': 3.28},
+
+    youtube transcripts are displayed like so
+    therefore to print only the text, we must only take the values of the dictionary whose key is "text"
+    '''
+
+    text = ""
+
+    for i in transcript:
+        text += i['text']+' '
+    print(text)
+    print('---------------------------------------------\n'*5)
+    #now text is just the raw transcript text
+    #now to summarise
+
+    summarizer = pipeline('summarization')
+    #pipeline cannot take giant chunks of text, hence we have to break it down into parts and merge the summarization together:
+
+    split_point = 1000
+    Splits = len(text)//split_point #summarize every 500 lines and append to final 
+    final_summarized = ""
+
+    final_summarized = []
+
+    for i in range(Splits+1):
+        start_point = i*split_point
+        end_point = (i+1)*split_point
+        line = summarizer(text[start_point:end_point])
+        line = line[0]
+        line = line['summary_text']
+        final_summarized.append(line)
+
+    print(final_summarized)
+    return final_summarized
+
